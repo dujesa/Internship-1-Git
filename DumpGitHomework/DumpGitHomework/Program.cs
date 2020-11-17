04e6d5113f08e0f52b0e9e7323259c06e61f02cb
dujesaric <dsaric02@fesb.hr>
2020-11-17 21:59:45 +0100

    Initial commit.

---
+++ DumpGitHomework/DumpGitHomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DumpGitHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git!");
+        }
+    }
+}
